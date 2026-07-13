32e8388e3667ef4f2ffac030f49d2c5df08f9777
Erny Salcedo <ernydavid04@gmail.com>
2026-07-12 20:12:01 -0500

    feat: remove unused SVG files and add new sparklie logo
    - Deleted `next.svg`, `vercel.svg`, and `window.svg` as they are no longer needed.
    - Added a new `sparklie-logo.svg` file to replace the removed assets.
    - Updated `tailwind.config.ts` to enable dark mode and adjust formatting for consistency.

---
+++ app/video/page.tsx
@@ -0,0 +1,10 @@
+import { YoutubePlaylist } from './youtube-playlist'
+
+export default function Page () {
+  return (
+    <div className='grid grid-cols-2 min-h-dvh'>
+      <p>Video Page</p>
+      <YoutubePlaylist youtubeLink='https://www.youtube.com/watch?v=OWhiCkEY-Yk&list=RDOWhiCkEY-Yk&start_radio=1' />
+    </div>
+  )
+}
---
+++ app/video/youtube-playlist.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React from 'react'
+import { LiteYTEmbed } from '@justinribeiro/lite-youtube'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'lite-youtube': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, LiteYTEmbed> & { videoid: string; playlistid?: string, autoload?: boolean, videoStartAt?: string, params?: string, posterquality?: string, className?: string };
+    }
+  }
+}
+
+export function YoutubePlaylist ({ youtubeLink }: {
+  youtubeLink?: string
+}) {
+  if (!youtubeLink) {
+    return (
+      <div className='h-full flex flex-col justify-center items-center'>
+        <p className='text-muted-foreground'>No video link or playlist</p>
+      </div>
+    )
+  }
+  const url = new URL(youtubeLink)
+
+  const videoId = url.searchParams.get('v')?.toString()
+  const playlistId = url.searchParams.get('list')?.toString()
+
+  if (videoId && playlistId) {
+    return (
+      <>
+        <lite-youtube
+          className='w-full h-full rounded-xl'
+          videoid={videoId}
+          playlistid={playlistId}
+          videoStartAt='5'
+          autoload
+          params='controls=2&enablejsapi=1&start_radio=1&autoplay=true'
+          posterquality='maxresdefault'
+        />
+        {/* <iframe width='560' height='315' src='https://www.youtube.com/embed/OWhiCkEY-Yk?si=DmtOf_w9gMcN7Zkp' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; cinnematics; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowFullScreen /> */}
+      </>
+    )
+  } else if (videoId) {
+    return (
+      <lite-youtube
+        className='w-full h-full rounded-xl'
+        videoid={videoId}
+        videoStartAt='5'
+        autoload
+        params='controls=0&enablejsapi=1&start_radio=1&autoplay=true'
+        posterquality='maxresdefault'
+      />
+    )
+  }
+}
